e18f7a41e8fe28688b5d3ef555296b0e45e18d09
Mohammad-akil <mohammadakilshakirhusain@gmail.com>
2023-05-25 13:11:17 +0530

    adding services

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TempratureDetails from "./components/TempratureDetails";
 import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/weatherService";
 const App = () => {
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({ q: "Baroda" });
+    console.log(data);
+  };
+  fetchWeather();
   return (
     <div className="mx-auto max-w-xl mt-4 py-5 px-4   bg-gradient-to-br from-cyan-700 to to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
